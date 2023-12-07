ed86207076f3e8e72e0f54ce54fc719c7394dc7d
Ben Lasscock <ben.lasscock@tgs.com>
2023-12-06 21:28:04 -0600

    enh:updated test

--- tensorstore/driver/zarr/driver_test.cc
+++ tensorstore/driver/zarr/driver_test.cc
@@ -132,9 +132,9 @@ std::string Bytes(std::vector<unsigned char> values) {
             "field": "a",
             "metadata": {
                 "compressor": {"id": "blosc"},
-                "dtype": [["a", "<i2"], ["b", "<i4"], ["c", "|V2"]],
-                "shape": [100, 100],
-                "chunks": [3, 2],
+                "dtype": [["a", "<i2"], ["b", "<i4"], ["c", "<i2"]],
+                "shape": [20, 20],
+                "chunks": [4, 4],
                 "dimension_separator": "/"
             }
         }
@@ -184,36 +184,116 @@ TEST(TENSORSTORE, STRUCTARRAY1) {
         tensorstore::OpenMode::open,
         tensorstore::ReadWriteMode::dynamic).result();
 
-    /// FIXME - check the output type of the store.
-    std::cout << store->dtype() << std::endl;
-    std::cout << store->domain() << std::endl;
+    using bytes_t = tensorstore::dtypes::byte_t;
 
-    std::filesystem::remove_all("test.zarr");
-}
+    auto bytes = tensorstore::dtype_v<bytes_t>;
 
+    // check it should have the right dtype
+    ASSERT_TRUE(store->dtype() == bytes);
+    EXPECT_THAT(store->domain().shape(), ::testing::ElementsAre(20, 20, 8));
+    
+    // slice for writing ...
+    auto slice_write = store | 
+        tensorstore::Dims({0, 1}).HalfOpenInterval(
+            {5,5}, {15, 15}, {1,1}
+        );
+    EXPECT_TRUE(slice_write.ok());
 
-TEST(TENSORSTORE, STRUCTARRAY2) {
-    // FIXME - make as test
-    auto create_store = tensorstore::Open(
-        GetJsonSpecStruct(),
-        tensorstore::OpenMode::create | tensorstore::OpenMode::open,
-        tensorstore::ReadWriteMode::dynamic)
-        .result();
+    //
+    #pragma pack(push, 1) // Disable padding
+    struct MyStruct {
+        int16_t a;
+        int32_t b;
+        int16_t c;
+    };
+    #pragma pack(pop) // Re-enable padding
 
-    std::cout << create_store->dtype() << std::endl;
-    std::cout << create_store->domain() << std::endl;
-    std::cout << " ----- \n\n" << std::endl;
+    tensorstore::Index N = 10;
+    // Create and populate a vector of MyStruct
+    std::vector<MyStruct> my_struct_array(N * N);
+    int idx = 0;
+    for (int i = 0; i < N; ++i) {
+        for (int j = 0; j < N; ++j) {
+            my_struct_array[idx] = MyStruct{
+                static_cast<int16_t>(i + j + 1), 
+                static_cast<int32_t>(i*2 + 2*j + 41), 
+                static_cast<int16_t>(i*3 + 3*j + 81)
+            };
+            ++idx;
+        }
+    }
+    // Now can we write bytes to it?
+    auto byte_array = tensorstore::AllocateArray<tensorstore::dtypes::byte_t>(
+        {N, N, 8}
+    );
 
-    auto as_byte_array = tensorstore::Open(
+    memcpy(
+        byte_array.data(),
+        my_struct_array.data(),
+        my_struct_array.size() * sizeof(MyStruct)
+    );
+    
+    auto write_result = tensorstore::Write(
+        byte_array, slice_write
+    );
+    write_result.commit_future.Wait();
+    write_result.copy_future.Wait();
+
+    EXPECT_TRUE(write_result.commit_future.result().ok());
+    EXPECT_TRUE(write_result.copy_future.result().ok());
+    
+    // now read something
+    
+    store = tensorstore::Open(
         GetJsonSpecStructBasic(),
         tensorstore::OpenMode::open,
         tensorstore::ReadWriteMode::dynamic).result();
+    
+    auto slice_read = store | 
+        tensorstore::Dims({0, 1}).TranslateHalfOpenInterval(
+            {0,0}, {10, 10}, {1,1}
+        );
+    EXPECT_TRUE(slice_read.ok());
 
-    /// FIXME - check the output type of the store.
-    std::cout << as_byte_array->dtype() << std::endl;
-    std::cout << as_byte_array->domain() << std::endl;
+    auto read_result = tensorstore::Read(slice_read.value()).result();
+    EXPECT_TRUE(read_result.ok());
 
+    std::vector<MyStruct> slice_struct_array(10 * 10);
+
+    memcpy(
+        slice_struct_array.data(),
+        read_result->data(),
+        slice_struct_array.size() * sizeof(MyStruct));
+
+    idx = 0;
+    for (int i = -5; i < 5; ++i) {
+        for (int j = -5; j < 5; ++j) {
+            if(i < 0 || j < 0){
+                EXPECT_TRUE(
+                    slice_struct_array[idx].a == 0
+                );
+                EXPECT_TRUE(
+                    slice_struct_array[idx].b == 0
+                );
+                EXPECT_TRUE(
+                    slice_struct_array[idx].c == 0
+                );               
+            } else {
+                EXPECT_TRUE(
+                    slice_struct_array[idx].a == i + j + 1
+                );
+                EXPECT_TRUE(
+                    slice_struct_array[idx].b == i*2 + 2*j + 41
+                );
+                EXPECT_TRUE(
+                    slice_struct_array[idx].c == i*3 + 3*j + 81
+                );
+            }
+            ++idx;
+        }
+    }
     std::filesystem::remove_all("test.zarr");
+
 }
 
 
